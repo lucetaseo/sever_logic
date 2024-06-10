ee1ea922940829844d18254e4194b7453ebd77fb
lucetaseo <101718150+lucetaseo@users.noreply.github.com>
2024-06-10 18:59:04 +0900

    크롬다이노 제작
    크롬다이노 기본으로 제작
    아이템 습득시 추가점수
    (추가 목표 캐릭터 변경 및 밤설정)

---
+++ src/init/assets.js
@@ -0,0 +1,45 @@
+// gameAssets.js
+import { rejects } from 'assert';
+import fs from 'fs';
+import path, { resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { callbackify } from 'util';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+const basePath = path.join(__dirname, '../../assets');
+
+
+const readFileAsync = (filename) => {
+  return new Promise((resolve, rejects)  => {
+    fs.readFile(path.join(basePath, filename), 'utf8',(err,data) => {
+      if(err){
+        rejects(err);
+        return;
+      }
+      resolve(JSON.parse(data));
+    });
+  });
+};
+
+let gameAssets = {};
+
+export const loadGameAssets = async () => {
+  try{
+  const[stage,item,item_unlock] = await Promise.all([
+    readFileAsync('stage.json'),
+    readFileAsync('item.json'),
+    readFileAsync('item_unlock.json'),
+  ]);
+
+  gameAssets = {stage, item, item_unlock};
+return gameAssets;
+} catch (e) {
+  throw new Error('Failed to load game assets: ' + e.message);
+}
+};
+
+export const getGameAssets = () => {
+  return gameAssets;
+}
---
+++ src/models/stage.model.js
@@ -0,0 +1,15 @@
+// key
+const stages = {};
+
+// 스테이지 초기화
+export const createStage = (uuid) => {
+  stages[uuid] = []; // 초기 스테이지 배열 생성
+};
+
+export const getStage = (uuid) => {
+  return stages[uuid];
+};
+
+export const setStage = (uuid, id, timestamp) => {
+  return stages[uuid].push({ id, timestamp });
+};
